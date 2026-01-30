3414d8597afc2555b1c7592c4e47826c6964a340
LuckedCoronet <lc45@lc-studios.net>
2026-01-30 13:15:40 +0900

    feat: update public exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
 // Public exports
+
+export {
+	PackTargetMode,
+	type BuildConfig,
+	type PackConfig,
+	type PackLayer,
+	type PackTarget,
+} from "./config";
+export { build, type BuildOptions } from "./entry";
