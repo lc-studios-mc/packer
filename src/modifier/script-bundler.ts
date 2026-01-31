b0189a335fee6250776c66685184974c4116b8f2
LuckedCoronet <lc45@lc-studios.net>
2026-01-31 11:46:22 +0900

    feat: add script bundler modifier

---
+++ src/modifier/script-bundler.ts
@@ -0,0 +1,71 @@
+import type { PackModifier } from "@/config";
+import { isFileUrl } from "@/utils";
+import esbuild from "esbuild";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+export type ScriptBundlerOptions = {
+	entry: string;
+	tsconfig?: string;
+	minify?: boolean;
+	sourceMap?: boolean;
+};
+
+export const createScriptBundlerModifier = (options: ScriptBundlerOptions): PackModifier => ({
+	name: "script-bundler",
+	apply: async ({ blueprint, packConfig }) => {
+		const entry = path.resolve(options.entry);
+		const outdir = path.join(packConfig.outDir, "scripts");
+		const sourceRoot = path.dirname(entry);
+		const tsconfig = options.tsconfig === undefined ? undefined : path.resolve(options.tsconfig);
+
+		let bundleOptions: esbuild.BuildOptions = {
+			entryPoints: [entry],
+			outdir,
+			tsconfig,
+			bundle: true,
+			minify: options.minify,
+			external: ["@minecraft"],
+			format: "esm",
+			platform: "node",
+			write: false, // Use custom write implementation
+		};
+
+		if (options.sourceMap) {
+			bundleOptions = {
+				...bundleOptions,
+				sourcemap: "linked",
+				sourceRoot,
+			};
+		}
+
+		const buildResult = await esbuild.build(bundleOptions);
+
+		for (const outputFile of buildResult.outputFiles ?? []) {
+			let content: string | Buffer;
+
+			if (path.extname(outputFile.path) === ".map") {
+				// Tweak source map contents to work with the Minecraft script debugger
+				const data = JSON.parse(outputFile.text);
+				const sources = data.sources as string[];
+				data.sources = sources.map((value) => {
+					const absPath = path.resolve(outdir, isFileUrl(value) ? fileURLToPath(value) : value);
+					const relativePath = path.relative(sourceRoot, absPath);
+					return relativePath;
+				});
+
+				content = JSON.stringify(data, null, 2);
+			} else {
+				content = outputFile.text;
+			}
+
+			const destPath = path.relative(packConfig.outDir, outputFile.path).split(path.sep).join("/");
+
+			blueprint.get(destPath).push({
+				kind: "buffer",
+				buffer: content,
+				encoding: "utf8",
+			});
+		}
+	},
+});
