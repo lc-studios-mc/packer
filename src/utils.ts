96121f83080d9f89acbd888817839dd1013f5748
LuckedCoronet <lc45@lc-studios.net>
2026-02-01 08:45:24 +0900

    feat: add util `globMap()`

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import path from "node:path";
+import picomatch from "picomatch";
 
 export const asArray = <T>(input: T | T[] | undefined | null): T[] => {
 	if (input === undefined || input === null) return [];
@@ -19,4 +20,21 @@ export const isFileUrl = (value: string): boolean => {
 
 export const isAbortError = (error: unknown): error is Error => {
 	return error instanceof Error && error.name === "AbortError";
+};
+
+export const globMap = <V>(
+	map: Map<string, V>,
+	glob: string | string[],
+	options?: picomatch.PicomatchOptions,
+): Map<string, V> => {
+	const matches = new Map<string, V>();
+	const isMatch = picomatch(glob, options);
+
+	for (const key of map.keys()) {
+		if (isMatch(key)) {
+			matches.set(key, map.get(key)!);
+		}
+	}
+
+	return matches;
 };
