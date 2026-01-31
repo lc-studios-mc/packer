b41fe53ccef92e49c4bcd43ddb2d4343a413044c
LuckedCoronet <lc45@lc-studios.net>
2026-01-31 11:50:28 +0900

    feat: update public exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 // Public exports
 
+export type { Blueprint } from "./blueprint";
 export { build, type BuildOptions } from "./build";
 export {
 	PackTargetMode,
 	type BuildConfig,
 	type PackConfig,
 	type PackLayer,
+	type PackModifier,
+	type PackModifierArgs,
+	type PackModifierFunction,
 	type PackTarget,
 } from "./config";
+export { EntrySourceKind, type EntrySource } from "./entry-source";
+export * as modifiers from "./modifier";
