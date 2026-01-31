65af43de2e9fb9e3247e179d6ef60cf6ddcfd49c
LuckedCoronet <lc45@lc-studios.net>
2026-01-31 11:45:37 +0900

    feat: add util `isFileUtil`

--- src/utils.ts
+++ src/utils.ts
@@ -8,6 +8,15 @@ export const asArray = <T>(input: T | T[] | undefined | null): T[] => {
 
 export const toPosixPath = (p: string) => p.split(path.sep).join("/");
 
+export const isFileUrl = (value: string): boolean => {
+	try {
+		const url = new URL(value);
+		return url.protocol === "file:";
+	} catch (e) {
+		return false; // Not a valid URL at all, so not a file URL
+	}
+};
+
 export const isAbortError = (error: unknown): error is Error => {
 	return error instanceof Error && error.name === "AbortError";
 };
