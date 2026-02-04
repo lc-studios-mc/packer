417416fb103917c7dd14f39986601e3b42d922c1
LuckedCoronet <lc45@lc-studios.net>
2026-02-04 10:19:50 +0900

    feat: update public exports

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,10 @@
 // Public exports
 
-export type { Blueprint } from "./blueprint";
-export { build, type BuildOptions } from "./build";
-export {
-	PackTargetMode,
-	type BuildConfig,
-	type PackConfig,
-	type PackLayer,
-	type PackPlugin,
-	type PackPluginArgs,
-	type PackPluginFunction,
-	type PackTarget,
-} from "./config";
-export { EntrySourceKind, type EntrySource } from "./entry-source";
+export * from "./blueprint";
+export * from "./build";
+export * from "./config";
+export * from "./entry-source";
+export * from "./plugins";
+export * from "./serializer";
+export * from "./types";
 export * from "./utils";
