8e8105f147fe8e0bc6cb6ad88c75c981a2f59783
LuckedCoronet <lc45@lc-studios.net>
2026-02-07 08:52:53 +0900

    feat!: remove built-in plugins

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ export * from "./blueprint";
 export * from "./build";
 export * from "./config";
 export * from "./entry-source";
-export * from "./plugins";
 export * from "./serializer";
 export * from "./types";
 export * from "./utils";
--- src/plugins/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./deserializer";
-export * from "./renamer";
-export * from "./script-bundler";
-export * from "./serializer";
